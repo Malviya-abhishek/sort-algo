1ba365a751099876255d6e49821609747f4e619a
abhishek <abhismalviya@outlook.com>
2021-04-02 18:50:32 +0530

    before leet

--- main.cpp
+++ main.cpp
@@ -1,7 +1,59 @@
-#include<bits/stdc++.h>
-#define ll long long 
+#include <bits/stdc++.h>
 
 using namespace std;
+
+vector<int> parents, ranks;
+
+int findParent(int x){
+    if(x == parents[x])
+        return x;
+    return parents[x] = findParent(parents[x]);
+}
+
+void Union(int x, int y){
+    int paX = findParent(x);
+    int paY = findParent(y);
+
+    if( paX == paY) 
+        return;
+
+    if( ranks[paY] > ranks[paX] )
+        parents[paX] = paY;
+    else
+        parents[paY] = paX;
+    
+    if(ranks[paX] == ranks[paY])
+        ranks[paX]++;
+}
+
+
+
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+    int n = isConnected.size();
+    
+    for(int i = 0 ; i < n ; ++i ){
+        parents.push_back(i);
+        ranks.push_back(0);
+    }
+
+    for(int i = 0 ; i < n ; ++i)
+        for(int j = i+1 ; j < n ; ++j)
+            if(isConnected[i][j])
+                Union(i,j);
+    
+    set<int> diff;
+
+    for(int i = 0 ; i < n ; ++i)
+        diff.insert( findParent( i ) );
+
+
+    
+    return diff.size();
+
+
+
+}
 
 int main(){
     ios::sync_with_stdio(false);
@@ -10,19 +62,19 @@ int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    
-    vector<int> v = {4,5,1,8,2,9,2,3,4};
-    int temp = 0;
-    int value = v[temp];
-    for(int i = 1 ; i < v.size() ; ++i){
-        if(v[i] <= value){
-            swap(v[i], v[temp]);
-            temp++;
-        }
-    }
 
-    for(int i : v)
-        cout<<i<<" ";
+    vector<vector<int>> isConnected = { 
+        {1,0,0,1},
+        {0,1,1,0},
+        {0,1,1,1},
+        {1,0,1,1}    
+    };
+
+
+    cout<<findCircleNum(isConnected)<<endl;
+
+
+    
 
 
 
